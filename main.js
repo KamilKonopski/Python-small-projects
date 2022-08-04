e543e06df3b006b61343db4cbc01cc4950b3bd30
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2022-08-04 22:16:13 +0200

    add validation

--- main.js
+++ main.js
@@ -16,18 +16,23 @@ const sendForm = event => {
         firstError.textContent = 'First Name cannot be empty';
         firstNameInput.classList.add('error-icon');
     } 
+
     if(lastNameInput.value === "") {
         lastError.textContent = 'Last Name cannot be empty';
         lastNameInput.classList.add('error-icon');
     } 
-    if(!emailAddressInput.value.match(validRegex)) {
+
+    if(emailAddressInput.value === "") {
+        emailError.textContent = 'Email address cannot be empty';
+        emailAddressInput.classList.add('error-icon');
+    }else if(!emailAddressInput.value.match(validRegex)) {
         emailError.textContent = 'Looks like this is not an email';
         emailAddressInput.classList.add('error-icon');
     }
+
     if(passwordInput.value === "") {
         passError.textContent = 'Password cannot be empty';
         passwordInput.classList.add('error-icon');
-        return
     }
 }
 
